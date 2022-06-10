18c5765f02b4eaff15848132f3bde0983589370b
Shakhana <shamilova.08@gmail.com>
2022-06-10 04:26:19 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        // входные данные
+        int price = 1500;
+
+        // логика
+
+        int bonusMileCost = 20;
+        int bonus = price / bonusMileCost;
+        System.out.println("Ваш бонус составляет: " + bonus + " миль ");
+    }
+
 }
